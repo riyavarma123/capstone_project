b6ac36414b69014dbabc7244710a28e3d7d97ecd
riyavarma123 <163863176+riyavarma123@users.noreply.github.com>
2024-11-04 22:35:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-
-# Define image name and tag
 IMAGE_NAME="ramya634/demo-website"
 IMAGE_TAG="latest"
-
-# Build Docker image
 echo "Building Docker image..."
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
-
-# Check if the image was built successfully
 if [[ $? -eq 0 ]]; then
     echo "Docker image built successfully: $IMAGE_NAME:$IMAGE_TAG"
 else
     echo "Docker image build failed."
     exit 1
 fi
-
